c7881aadc4a9ec5552411c47acb835d79a9dcb94
jey707 <jey707@naver.com>
2022-07-14 18:23:22 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
 const mainBg = document.querySelector(".main-bg");
 const mainBgImgWidth = window.innerWidth;
-console.log(mainBgImgWidth);
+const enterIntroImg = document.querySelector(".enter-intro-img");
+const enterImgHover = document.querySelector(".intro-img-hover");
+let lastScroll = 0;
+let i = 0;
+// console.log(window.pageYOffset);
+// console.log(mainBgImgWidth);
 mainBg.style.width = mainBgImgWidth;
 // window.onload = function (e) {
 
 // };
-
+window.onload = function () {
+  // setTimeout(function () {
+  //   scrollTo(0, 0);
+  // }, 100);
+};
 window.addEventListener("scroll", function (e) {
-  console.log(e);
-  console.log(window.scrollY);
+  bgImageX(window.scrollY);
 });
+
+function bgImageX(Y) {
+  if (Y > lastScroll && Y < 700) {
+    i -= 1;
+    mainBg.style.backgroundPosition = `${i}px center`;
+  } else if (Y < lastScroll) {
+    i += 1;
+    mainBg.style.backgroundPosition = `${i}px center`;
+  }
+  lastScroll = Y;
+}
