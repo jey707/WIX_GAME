ef523ae0b4a17ebb0c83c23692c4ca47e8e65349
jey707 <jey707@naver.com>
2022-07-19 21:27:40 +0900

    update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const mainBg = document.querySelector(".main-bg");
 const mainBgImgWidth = window.innerWidth;
 const enterIntroImg = document.querySelector(".enter-intro-img");
 const enterImgHover = document.querySelector(".intro-img-hover");
+const gmBloc = document.querySelector(".game-intro-back");
+const rext = gmBloc.getBoundingClientRect();
+
 let lastScroll = 0;
 let i = 0;
 // console.log(window.pageYOffset);
@@ -15,11 +18,16 @@ window.onload = function () {
   //   scrollTo(0, 0);
   // }, 100);
 };
+
 window.addEventListener("scroll", function (e) {
   bgImageX(window.scrollY);
+  plantopacity(window.scrollY);
+  // console.log(e);
 });
 
 function bgImageX(Y) {
+  // console.log("Y= ", Y);
+  // console.log("lastscroll= ", lastScroll);
   if (Y > lastScroll && Y < 700) {
     i -= 1;
     mainBg.style.backgroundPosition = `${i}px center`;
@@ -27,5 +35,22 @@ function bgImageX(Y) {
     i += 1;
     mainBg.style.backgroundPosition = `${i}px center`;
   }
+  if (Y > 1600 && Y < 2400) {
+    // console.log("증가");
+  } else if (Y < 1600 && Y > 2400) {
+    // console.log("감소");
+  }
   lastScroll = Y;
+}
+function plantopacity(Y) {
+  console.log("rectY : ", rext.y);
+  console.log(
+    "clientH : ",
+    document.querySelector(".game-intro-back").clientHeight
+  );
+  // console.log("inHegt : ", window.innerHeight);
+  // console.log("scrollY : ", window.scrollY);
+  // console.log("scrollY : ", parseFloat((window.scrollY / 3500).toPrecision(4)));
+  const opc = parseFloat((Y / 4000).toPrecision(4));
+  gmBloc.style.opacity = `${opc}`;
 }
